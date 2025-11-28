d3ca4ca731e2b353046463835788e3c95c8bf0d9
masudibnmusa <masudibnmusa10@gmail.com>
2025-11-28 23:23:10 +0600

    add8

--- pickaboo_db.sql
+++ pickaboo_db.sql
@@ -55,3 +55,25 @@ CREATE TABLE order_items (
     FOREIGN KEY (order_id) REFERENCES orders(order_id),
     FOREIGN KEY (product_id) REFERENCES products(product_id)
 );
+
+INSERT INTO customers (name, email, phone, address, district) VALUES
+('Masud Ibn Musa', 'masud@example.com', '01710000001', 'Katiadi', 'Kishoreganj'),
+('Rakib Hossain', 'rakib@example.com', '01710000002', 'Moshua', 'Dhaka'),
+('Sajid Ahmed', 'sajid@example.com', '01710000003', 'Mirpur', 'Dhaka'),
+('Fahim Khan', 'fahim@example.com', '01710000004', 'Uttara', 'Dhaka'),
+('Siam Ali', 'siam@example.com', '01710000005', 'Banani', 'Dhaka'),
+('Rana Hossain', 'rana@example.com', '01710000006', 'Gulshan', 'Dhaka'),
+('Arif Chowdhury', 'arif@example.com', '01710000007', 'Motijheel', 'Dhaka'),
+('Shamim Ahmed', 'shamim@example.com', '01710000008', 'Narsingdi', 'Narsingdi'),
+('Tanvir Hossain', 'tanvir@example.com', '01710000009', 'Comilla', 'Comilla'),
+('Jahid Alam', 'jahid@example.com', '01710000010', 'Chittagong', 'Chattogram'),
+('Imran Sarker', 'imran@example.com', '01710000011', 'Khulna', 'Khulna'),
+('Rafiq Islam', 'rafiq@example.com', '01710000012', 'Barishal', 'Barishal'),
+('Nayeem Khan', 'nayeem@example.com', '01710000013', 'Sylhet', 'Sylhet'),
+('Arman Hossain', 'arman@example.com', '01710000014', 'Jessore', 'Jessore'),
+('Sabbir Rahman', 'sabbir@example.com', '01710000015', 'Pabna', 'Pabna'),
+('Shuvo Das', 'shuvo@example.com', '01710000016', 'Dinajpur', 'Dinajpur'),
+('Tariq Mahmud', 'tariq@example.com', '01710000017', 'Rangpur', 'Rangpur'),
+('Foysal Ahmed', 'foysal@example.com', '01710000018', 'Cox’s Bazar', 'Cox’s Bazar'),
+('Azizur Rahman', 'azizur@example.com', '01710000019', 'Tangail', 'Tangail'),
+('Hasan Mahmud', 'hasan@example.com', '01710000020', 'Bogra', 'Bogra');
