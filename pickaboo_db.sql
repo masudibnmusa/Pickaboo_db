cadadcaef236d88d98ba19fb75266c6b247d7c8a
masudibnmusa <masudibnmusa10@gmail.com>
2025-11-30 22:31:44 +0600

    ad3

--- pickaboo_db.sql
+++ pickaboo_db.sql
@@ -285,3 +285,8 @@ FROM products p
 JOIN order_items oi ON p.product_id = oi.product_id
 GROUP BY p.product_name
 ORDER BY sold_units DESC;
+
+SELECT b.brand_name, COUNT(p.product_id) AS product_count
+FROM brands b
+LEFT JOIN products p ON b.brand_id = p.brand_id
+GROUP BY b.brand_name;
