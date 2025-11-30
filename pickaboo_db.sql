9a2680d311d1a284971af04657f3da92903fc3b8
masudibnmusa <masudibnmusa10@gmail.com>
2025-11-30 22:32:26 +0600

    ad4

--- pickaboo_db.sql
+++ pickaboo_db.sql
@@ -290,3 +290,9 @@ SELECT b.brand_name, COUNT(p.product_id) AS product_count
 FROM brands b
 LEFT JOIN products p ON b.brand_id = p.brand_id
 GROUP BY b.brand_name;
+
+SELECT c.name, COUNT(o.order_id) AS total_orders
+FROM customers c
+LEFT JOIN orders o ON c.customer_id = o.customer_id
+GROUP BY c.name;
+
