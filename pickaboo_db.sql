f76996a7f3fa9885a56c4d6a08d283eaec4883f9
masudibnmusa <masudibnmusa10@gmail.com>
2025-11-30 22:34:53 +0600

    ad5

--- pickaboo_db.sql
+++ pickaboo_db.sql
@@ -296,3 +296,8 @@ FROM customers c
 LEFT JOIN orders o ON c.customer_id = o.customer_id
 GROUP BY c.name;
 
+SELECT c.name, o.order_id, p.product_name, oi.quantity
+FROM customers c
+JOIN orders o ON c.customer_id = o.customer_id
+JOIN order_items oi ON o.order_id = oi.order_id
+JOIN products p ON oi.product_id = p.product_id;
