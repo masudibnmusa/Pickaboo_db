0949a9093347e3f4f324b666b39b7e55d03869d6
masudibnmusa <masudibnmusa10@gmail.com>
2025-12-04 22:55:38 +0600

    ad6

--- pickaboo_db.sql
+++ pickaboo_db.sql
@@ -683,4 +683,9 @@ SELECT p.product_name, c.category_name
 FROM products p
 JOIN categories c ON p.category_id = c.category_id;
 
+SELECT p.product_name, b.brand_name, c.category_name
+FROM products p
+JOIN brands b ON p.brand_id = b.brand_id
+JOIN categories c ON p.category_id = c.category_id;
+
 
