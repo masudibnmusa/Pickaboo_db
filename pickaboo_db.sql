7f426dc95c3cf97a63dd78781f9853958f0a4688
masudibnmusa <masudibnmusa10@gmail.com>
2025-12-04 22:51:39 +0600

    ad1

--- pickaboo_db.sql
+++ pickaboo_db.sql
@@ -662,4 +662,9 @@ FROM customers c
 JOIN orders o ON c.customer_id = o.customer_id
 JOIN order_items oi ON o.order_id = oi.order_id
 JOIN products p ON oi.product_id = p.product_id;
+use pickabo_db;
+SELECT c.name, o.order_id, o.total_amount
+FROM customers c
+JOIN orders o ON c.customer_id = o.customer_id;
+
 
