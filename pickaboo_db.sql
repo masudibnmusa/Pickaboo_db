33a9560d44fcacc8166faf08ce4c45c1074c26eb
masudibnmusa <masudibnmusa10@gmail.com>
2025-12-05 22:47:42 +0600

    ad14

--- pickaboo_db.sql
+++ pickaboo_db.sql
@@ -811,3 +811,10 @@ FROM brands b
 JOIN products p ON b.brand_id = p.brand_id
 JOIN order_items oi ON p.product_id = oi.product_id
 GROUP BY b.brand_id;
+
+SELECT c.name, SUM(o.total_amount) AS total_spent
+FROM customers c
+JOIN orders o ON c.customer_id = o.customer_id
+GROUP BY c.customer_id
+ORDER BY total_spent DESC
+LIMIT 1;
