ab2f20d9c4b0f0bb8e01d9de9d2951a4923878e0
masudibnmusa <masudibnmusa10@gmail.com>
2025-12-05 22:44:31 +0600

    ad9

--- pickaboo_db.sql
+++ pickaboo_db.sql
@@ -783,3 +783,7 @@ SELECT p.product_id, p.product_name
 FROM products p
 LEFT JOIN order_items oi ON p.product_id = oi.product_id
 WHERE oi.order_id IS NULL;
+
+SELECT DISTINCT p.product_name
+FROM products p
+JOIN order_items oi ON p.product_id = oi.product_id;
