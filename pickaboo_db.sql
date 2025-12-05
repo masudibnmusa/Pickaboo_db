e52ed7efd1b55e7fb13e0168302da9ef737af381
masudibnmusa <masudibnmusa10@gmail.com>
2025-12-05 22:44:00 +0600

    ad8

--- pickaboo_db.sql
+++ pickaboo_db.sql
@@ -778,3 +778,8 @@ SELECT c.customer_id, c.name, COUNT(o.order_id) AS order_count
 FROM customers c
 LEFT JOIN orders o ON c.customer_id = o.customer_id
 GROUP BY c.customer_id;
+
+SELECT p.product_id, p.product_name
+FROM products p
+LEFT JOIN order_items oi ON p.product_id = oi.product_id
+WHERE oi.order_id IS NULL;
