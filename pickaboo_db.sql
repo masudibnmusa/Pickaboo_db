9e44e23792c146359c4efaa02f44bb9bf788b8ce
masudibnmusa <masudibnmusa10@gmail.com>
2025-12-06 21:20:27 +0600

    ad5

--- pickaboo_db.sql
+++ pickaboo_db.sql
@@ -847,3 +847,10 @@ JOIN order_items oi ON p.product_id = oi.product_id
 GROUP BY cat.category_id
 ORDER BY total_qty DESC
 LIMIT 1;
+
+SELECT c.name, o.order_id, p.product_name, cat.category_name
+FROM customers c
+JOIN orders o ON c.customer_id = o.customer_id
+JOIN order_items oi ON o.order_id = oi.order_id
+JOIN products p ON oi.product_id = p.product_id
+JOIN categories cat ON p.category_id = cat.category_id;
