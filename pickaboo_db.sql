bebc2314b2174b523dd5d30f3f195cb28415948d
masudibnmusa <masudibnmusa10@gmail.com>
2025-12-06 21:25:10 +0600

    ad12

--- pickaboo_db.sql
+++ pickaboo_db.sql
@@ -889,3 +889,16 @@ SELECT o.order_id, SUM(oi.quantity * oi.price) AS total_revenue
 FROM orders o
 JOIN order_items oi ON o.order_id = oi.order_id
 GROUP BY o.order_id;
+
+SELECT 
+    o.order_id,
+    p.product_name,
+    b.brand_name,
+    c.category_name,
+    oi.quantity,
+    oi.price
+FROM orders o
+JOIN order_items oi ON o.order_id = oi.order_id
+JOIN products p ON oi.product_id = p.product_id
+JOIN brands b ON p.brand_id = b.brand_id
+JOIN categories c ON p.category_id = c.category_id;
