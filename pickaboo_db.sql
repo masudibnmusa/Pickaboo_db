aac4d2375ed7a5fc9f6cc4383ba6d4229c5cac9d
masudibnmusa <masudibnmusa10@gmail.com>
2025-12-06 21:19:16 +0600

    ad3

--- pickaboo_db.sql
+++ pickaboo_db.sql
@@ -833,3 +833,9 @@ SELECT o.order_id, SUM(oi.quantity) AS total_items
 FROM orders o
 JOIN order_items oi ON o.order_id = oi.order_id
 GROUP BY o.order_id;
+
+SELECT c.name, o.order_id, p.product_name, oi.quantity
+FROM customers c
+JOIN orders o ON c.customer_id = o.customer_id
+JOIN order_items oi ON o.order_id = oi.order_id
+JOIN products p ON oi.product_id = p.product_id;
